c7bc7c4a67d23527e5d416ee9db0b9d9651b1049
Bilal Raja <rajabilal555@gmail.com>
2024-11-08 23:19:14 +0500

    feat: added project modal to home

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,8 +1,8 @@
-import {Head, Link} from "@inertiajs/react";
-import {PageProps} from "@/types";
+import { Head, Link } from "@inertiajs/react";
+import { PageProps } from "@/types";
 import BaseLayout from "@/Layouts/BaseLayout";
-import {MdWavingHand} from "react-icons/md";
-import {FiChevronRight} from "react-icons/fi";
+import { MdWavingHand } from "react-icons/md";
+import { FiChevronRight } from "react-icons/fi";
 import Divider from "@/Components/Divider";
 import TechCard from "@/Components/Home/TechCard";
 import SecondaryButton from "@/Components/Buttons/SecondaryButton";
@@ -12,17 +12,20 @@ import ProjectCard from "@/Components/Project/ProjectCard";
 import LinkButton from "@/Components/Buttons/LinkButton";
 import DynamicFAIcon from "@/Components/Icons/DynamicFAIcons";
 import Markdown from "react-markdown";
-import {mdC, mdPrimaryStrong} from "@/lib/markdown-utils";
+import { mdC, mdPrimaryStrong } from "@/lib/markdown-utils";
+import Modal from "@/Components/Modal";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import ProjectDetailsModal from "@/Components/Project/ProjectDetailsModal";
 
-export default function Home({portfolio}: PageProps) {
+export default function Home({ portfolio }: PageProps) {
     return (
         <>
-            <Head title={`<${portfolio.name} />`}/>
+            <Head title={`<${portfolio.name} />`} />
             <BaseLayout>
                 <div className="flex flex-col w-full">
                     <h1 className="flex items-end gap-2 text-4xl font-[600] text-white group/title">
-                        Hi, I'm {portfolio.first_name} <MdWavingHand
-                        className="motion-safe:animate-bounce text-primary"/>
+                        Hi, I'm {portfolio.first_name}{" "}
+                        <MdWavingHand className="motion-safe:animate-bounce text-primary" />
                         <span className="text-sm font-normal text-slate-400">
                             aka {`<${portfolio.nick_name} />`}
                         </span>
@@ -33,37 +36,39 @@ export default function Home({portfolio}: PageProps) {
                     <div className="flex flex-row gap-2 mt-4">
                         {/* Social Links */}
                         {portfolio.links.map((link) => (
-                            <a
-                                key={link.url}
-                                href={link.url}
-                                target="_blank"
-                            >
+                            <a key={link.url} href={link.url} target="_blank">
                                 <SecondaryButton>
                                     {/*<FaLinkedin size={20}/>*/}
-                                    <DynamicFAIcon icon={link.icon} size="20"/>
+                                    <DynamicFAIcon icon={link.icon} size="20" />
                                     {link.name}
                                 </SecondaryButton>
                             </a>
                         ))}
                     </div>
-                    <Markdown className="mt-4 text-justify" components={mdC([mdPrimaryStrong])}>
+                    <Markdown
+                        className="mt-4 text-justify"
+                        components={mdC([mdPrimaryStrong])}
+                    >
                         {portfolio.short_about}
                     </Markdown>
-                    <Divider/>
+                    <Divider />
 
-                    <HomeTimeline portfolio={portfolio}/>
-                    <Divider className="mt-0"/>
+                    <HomeTimeline portfolio={portfolio} />
+                    <Divider className="mt-0" />
 
                     <HomeSection title="Some of my Projects">
                         <div className="flex flex-col gap-6">
                             {portfolio.projects.map((project) => (
-                                <ProjectCard
+                                <Modal
                                     key={project.id}
-                                    project={project}
-                                />
+                                    trigger={<ProjectCard project={project} />}
+                                >
+                                    <DialogTitle>{project.title}</DialogTitle>
+                                    <ProjectDetailsModal project={project} />
+                                </Modal>
                             ))}
                             <div className="flex w-full">
-                                <Link href={route('about')}>
+                                <Link href={route("about")}>
                                     <LinkButton className="group">
                                         View all Projects
                                         <FiChevronRight
@@ -80,11 +85,14 @@ export default function Home({portfolio}: PageProps) {
                     <HomeSection title="My evolving skillset">
                         <div className="grid grid-cols-3 gap-4 mx-6 md:grid-cols-5 text-primary">
                             {portfolio.skills.map((skill) => (
-                                <TechCard key={skill.id} iconUrl={skill.icon_url} techName={skill.name}/>
+                                <TechCard
+                                    key={skill.id}
+                                    iconUrl={skill.icon_url}
+                                    techName={skill.name}
+                                />
                             ))}
                         </div>
                     </HomeSection>
-
                 </div>
             </BaseLayout>
         </>
